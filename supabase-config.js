2d22840cbe77f8121de8ca03a27e2ee46412d1f9
buyhaat <forhad93706@gmail.com>
2026-09-18 17:40:04 +0600

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,13 @@
-// তোমার নিজের Supabase values এখানে বসাও
-window.SUPABASE_URL = "https://kppqrhqkwxwloootpmuc.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_LbOwpkT5uf25H-A89P9ZGg_Zt7GP4ke";";
-window.supabaseClient=window.supabase.createClient(window.SUPABASE_URL,window.SUPABASE_ANON_KEY);
+// BuyHaat — Supabase Configuration
+
+window.SUPABASE_URL =
+  "https://kppqrhqkwxwloootpmuc.supabase.co";
+
+window.SUPABASE_ANON_KEY =
+  "sb_publishable_LbOwpkT5uf25H-A89P9ZGg_Zt7GP4ke";
+
+window.supabaseClient =
+  window.supabase.createClient(
+    window.SUPABASE_URL,
+    window.SUPABASE_ANON_KEY
+  );
